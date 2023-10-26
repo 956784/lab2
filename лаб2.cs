b77d8c2579934cd3d7f0edf3721ba9211b1c10f3
956784 <146649071+956784@users.noreply.github.com>
2023-10-26 23:56:34 +0300

    Add files via upload

---
+++ лаб2.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите 2 числа");
+double x = Convert.ToDouble(Console.ReadLine());
+double y = Convert.ToDouble(Console.ReadLine());
+double z = (x + y) / 2;
+double w = 2 * (x * y);
+if (x > y)
+{
+    x = w;
+    y = z;
+    Console.WriteLine($"x = {x} y = {y}");
+}
+else if (y > x)
+{
+    y = w;
+    x = z;
+    Console.WriteLine($"x = {x} y = {y}");
+}
+else
+{
+    Console.WriteLine("Ошибка");
+}
